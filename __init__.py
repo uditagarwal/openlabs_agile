4767f43fcb7be60f3c06e171fb861cc29b63305c
Udit Agarwal <udit.agarwal@openlabs.co.in>
2013-03-11 14:37:46 +0530

    Created Boilerplates
    * __init__.py, tryton.cfg, setup.py created
    * Changes still need to be made to setup.py
    * Dependencies in tryton.cfg for party, timesheet, company

---
+++ __init__.py
@@ -0,0 +1,18 @@
+:# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Initialization for Agile Module of Project Module
+
+    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+
+from trytond.pool import Pool
+from .agile import *
+
+def register():
+    Pool.register(
+        Agile,
+        module='project', type_='model'
+    )
