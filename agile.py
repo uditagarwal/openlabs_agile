9ad882a25b07b17237e0baa6f26fbca67ac200ca
Udit Agarwal <udit.agarwal@openlabs.co.in>
2013-03-11 14:42:30 +0530

    Added empty agile.py and agile.xml
    *Nothing in them yet

---
+++ agile.py
@@ -0,0 +1,10 @@
+:# -*- coding: utf-8 -*-
+"""
+    Contains Code for Agile part of Project Module
+
+    Implements allowing Agile Development through Projects
+
+    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+
